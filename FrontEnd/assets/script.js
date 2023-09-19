7b4fcb35a53d4c0bea2f47c3ddc2045ae8e553f7
Noosly <139576679+Noosly@users.noreply.github.com>
2023-09-19 12:06:26 +0200

    Ajouter  la photo dans la Gallery (div_gallery)

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -249,15 +249,20 @@ function send_new_photo(ev){
             //transform the response to JSON          
             let  res = JSON.parse(req.response);   
             //Update the Gallery
-            gallery_list = document.querySelector(".photos-gallery__list");  
+            gallery_list_modal = document.querySelector(".photos-gallery__list");  
             alert(`modal_window_photo_${res.id}`);            
-            gallery_list.insertAdjacentHTML('beforeend', `<figure id="modal_window_figure_${res.id}">            
+            gallery_list_modal.insertAdjacentHTML('beforeend', `<figure id="modal_window_figure_${res.id}">            
                                             <div >
                                                 <svg id="modal_window_photo_${res.id}"  work-id="${res.id}" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
                                             </div>                
                                             <img src="${res.imageUrl}" alt="${res.imageUrl}">
                                             <figcaption>éditer</figcaption>
                                         </figure>`);    
+            div_gallery.insertAdjacentHTML('beforeend', `<figure id="gallery_photo_${res.id}">       
+                                                       
+                                        <img src="${res.imageUrl}" alt="${res.imageUrl}">
+                                        <figcaption>éditer</figcaption>
+                                    </figure>`);    
         }
         else{
             alert(`Erreur ${req.status} lors de l'envoie de la gallerie`);
