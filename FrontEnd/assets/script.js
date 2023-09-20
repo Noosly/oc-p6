0595d2fe7f666d6d869233cdfa3540f2cd327400
Noosly <139576679+Noosly@users.noreply.github.com>
2023-09-20 20:42:47 +0200

    Supression des espaces inutiles et des commentaires inutiles

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -142,10 +142,6 @@ async function delete_work(works_url, id_work){
     } 
 }
 
-
-
-
-
 function add_event_listenerer_to_modal_window_photo(works, works_url){
     for(let w of works){
         let modal_window_photo = document.querySelector(`#modal_window_photo_${w.id}`);
@@ -213,7 +209,6 @@ function close_modal_window(){
     modal_window.style.display = 'none';
     gallery.style.display = 'none';
     add_photo.style.display = 'none';
-    //location.reload();//A changer...
 }
 
 function browse_photo(){
@@ -251,8 +246,7 @@ function send_new_photo(ev){
             //transform the response to JSON          
             let  res = JSON.parse(req.response);   
             //Update the Gallery
-            gallery_list_modal = document.querySelector(".photos-gallery__list");  
-            //alert(`modal_window_photo_${res.id}`);            
+            gallery_list_modal = document.querySelector(".photos-gallery__list");           
             gallery_list_modal.insertAdjacentHTML('beforeend', `<figure id="modal_window_figure_${res.id}">            
                                             <div >
                                                 <svg id="modal_window_photo_${res.id}"  work-id="${res.id}" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
@@ -318,7 +312,6 @@ div_edition_projects.addEventListener('click', open_modal_window);
 exit_photos_gallery.addEventListener('click', close_modal_window);
 exit_add_photo.addEventListener('click', close_modal_window);
 btn_browse_photo.addEventListener('change', browse_photo);
-
 form_new_photo.addEventListener('submit', send_new_photo); 
 
 
